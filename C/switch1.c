c5a4caff3c45f77c8deb8016b4757dd1a7cfa945
SanketT22 <105072019+SanketT22@users.noreply.github.com>
2025-07-10 07:24:11 +0000

    sw

--- C/switch1.c
+++ C/switch1.c
@@ -2,21 +2,46 @@
 int main()
 {
     int ch;
-    printf("enter the number");
+    int a,b,total;
+    printf("\n1 for Addition \n2 for subtraction \n3 for multiplication \n4 for division");
     scanf("%d",&ch);
 
     switch(ch)
     {
         case 1:
-            printf("\n monday\n");
+            printf("\n enter first number\n");
+            scanf("%d",&a);
+            printf("\n enter second number\n");
+            scanf("%d",&b);
+            total=a+b;
+            printf("\nAddition is %d \n",total);
         break;
 
         case 2:
-            printf("\ntuesday\n");
+             printf("\n enter first number\n");
+            scanf("%d",&a);
+            printf("\n enter second number\n");
+            scanf("%d",&b);
+            total=a-b;
+            printf("\n subtraction is %d \n",total);
         break;
 
         case 3:
-            printf("\nwed\n");
+            printf("\n enter first number\n");
+            scanf("%d",&a);
+            printf("\n enter second number\n");
+            scanf("%d",&b);
+            total=a*b;
+            printf("\n multiplication is %d \n",total);
+        break;
+
+        case 4:
+         printf("\n enter first number\n");
+            scanf("%d",&a);
+            printf("\n enter second number\n");
+            scanf("%d",&b);
+            total=a/b;
+            printf("\n divison is %d \n",total);
         break;
         default:
         printf("\ninvalid day\n");
